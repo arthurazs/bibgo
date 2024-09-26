804763c74a61ea9f534b89c5325073cbae18dc68
Arthur Zopellaro <github@zope.me>
2024-09-26 17:11:37 -0300

    chore: add TODO

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -6,6 +6,9 @@ import (
 )
 
 func main() {
+    // TODO add tree walking
+    // TODO add elapsed time
+    // TODO add logger
 	counter := bibgo.ParseFile("data/input/acm/acm.bib")
 	fmt.Printf("Parsed %d entries\n", counter)
 }
