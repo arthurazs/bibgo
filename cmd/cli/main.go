b6f4581654a37718415da2c7d8df2e307e24911b
Arthur Zopellaro <github@zope.me>
2024-09-28 21:29:27 -0300

    feat: add dir walking

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -3,12 +3,32 @@ package main
 import (
 	"bibgo"
 	"fmt"
+	"os"
+	"path/filepath"
 )
 
 func main() {
-    // TODO add tree walking
-    // TODO add elapsed time
-    // TODO add logger
-	counter := bibgo.ParseFile("data/input/acm/acm.bib")
-	fmt.Printf("Parsed %d entries\n", counter)
+	// TODO add elapsed time
+	// TODO add logger
+	counter := uint64(0)
+
+	path := filepath.Join("data", "input")
+	dirs, err := os.ReadDir(path)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Opening folders...")
+	for _, dir := range dirs {
+		subFilepath := filepath.Join(path, dir.Name())
+		fmt.Printf("Opening %s...\n", subFilepath)
+		subdirs, err := os.ReadDir(subFilepath)
+		if err != nil {
+			panic(err)
+		}
+		for _, dir = range subdirs {
+			counter += bibgo.ParseFile(filepath.Join(subFilepath, dir.Name()))
+		}
+	}
+	fmt.Printf("Took     ? ms to parse %d entries\n", counter)
 }
